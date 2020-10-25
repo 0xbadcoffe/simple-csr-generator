19ba0726295edab6c9e556e44c108736de6e4d22
Heqing Huang <feipenghhq@gmail.com>
2020-10-24 18:00:29 -0700

    main.py and makefile
        * main.py - main function
        * makefile - to run rtl compilation and lint

--- tool/main.py
+++ tool/main.py
@@ -16,13 +16,22 @@ sys.dont_write_bytecode = True
 import argparse
 from RegParser import RegParser
 from HtmlWriter import HtmlWriter
+from VerilogWriter import VerilogWriter
 import os
 from shutil import rmtree
+
+def createDir(path):
+    """ Create directory """
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        rmtree(path)
+        os.makedirs(path)
 
 # Parse the argument
 parser = argparse.ArgumentParser(description='Simple CSR Generator.')
 parser.add_argument('yml', type=str,
-                    help='yml file defining containing information')
+                    help='yml file containing register information')
 parser.add_argument('-outdir', type=str,
                     help='Output directory name')
 args = parser.parse_args()
@@ -32,18 +41,20 @@ outdir = args.outdir
 fileName = os.path.basename(yml)
 filePath = os.path.dirname(yml)
 moduleName = fileName.replace('.yml', '')
-path =  filePath + '/' + moduleName + '_csr' if (outdir == None) else outdir
+rtlPath =  filePath + '/' + moduleName + '_csr/rtl' if (outdir == None) else outdir
+docPath =  filePath + '/' + moduleName + '_csr/doc' if (outdir == None) else outdir
 
-try:
-    os.mkdir(path)
-except FileExistsError:
-    rmtree(path)
-    os.mkdir(path)
+createDir(rtlPath)
+createDir(docPath)
 
 # Parse the yml file
 parser = RegParser(yml)
 regsInfo = parser.parserAllReg()
 
 # Write the HTML document
-hwriter = HtmlWriter(regsInfo, moduleName, path)
+hwriter = HtmlWriter(regsInfo, moduleName, docPath)
 hwriter.writeAll()
+
+# Write the  Verilog File
+vwriter = VerilogWriter(regsInfo, moduleName, rtlPath)
+vwriter.writeVerilog()
